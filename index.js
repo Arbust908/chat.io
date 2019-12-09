94ac213ba461724df732d62f66bb10973b17eccc
arbust908@gmail.com <Arbust908@gmail.com>
2019-12-09 20:32:10 -0300

    Agregado de quien escribe, quien online y nombre del mensaje

--- index.js
+++ index.js
@@ -14,23 +14,40 @@ http.listen(3000, () =>{
 });
 
 io.on('connection', (socket) => {
+    //Avisa que hay una nueva coneccion
     console.log('there is a connection');
+    //Avisa que se perdio una coneccion
     socket.on('disconnect', () => {
         console.log('Bye bye');
     });
+
     socket.on('Creado', (data) => {
         //io.emit('Creado', data); // <-- este emite a todos
         socket.broadcast.emit('Creado', data); // <-- Este solo emite a otros
     });
-    socket.on('chatMessage', (data) => {
-        console.log('Emito ' + data);
-        
-        socket.broadcast.emit('chatMessage', (data));
+
+    socket.on('chatMessage', (msg) => {
+        // console.log(msg);
+        socket.broadcast.emit('chatMessage', (msg));
     });
-    socket.on('typing', () => {
-        socket.broadcast.emit('typing');
+
+    socket.on('typing', (name) => {
+        socket.broadcast.emit('typing', name);
     });
-    socket.on('stopedTyping', () => {
-        socket.broadcast.emit('stopedTyping');
+
+    socket.on('stopedTyping', (name) => {
+        socket.broadcast.emit('stopedTyping', name);
+    });
+
+    socket.on('joined', (data) => {
+        socket.broadcast.emit('joined', (data));
+    });
+
+    socket.on('whosOnline', (data) => {
+        socket.broadcast.emit('whosOnline', (data));
+    });
+
+    socket.on('leave', (data) => {
+        socket.broadcast.emit('leave', (data));
     });
 });
